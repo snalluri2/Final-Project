6701721f73220fe72dd873b9cab58fe8b2f1fd0c
snalluri2 <srikarnllr@gmail.com>
2021-12-12 23:47:14 -0500

    task :- Making Changes

--- calc/calculations/multiplication.py
+++ calc/calculations/multiplication.py
@@ -2,9 +2,7 @@
 from calc.calculations.calculation import Calculation
 
 class Multiplication(Calculation):
-    """subtraction calculation object"""
     def get_result(self):
-        """get the multiplication results"""
         result = 1.0
         for value in self.values:
             result = result * value
